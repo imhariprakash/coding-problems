1050bd8a108e32c6b3c67a7a811654f505b1b192
imhariprakash <58242393+imhariprakash@users.noreply.github.com>
2024-07-22 20:57:32 +0530

    problem notes and learnings

--- problems/src/string/SoftwareVersionComparison.java
+++ problems/src/string/SoftwareVersionComparison.java
@@ -11,6 +11,8 @@ public class SoftwareVersionComparison
 
     public static void compareVersions()
     {
+        System.out.println("0: Both are same 1: First version is greater. -1: Second version is greater");
+        System.out.println(isTheSameVersion("1.0", "1.0.0"));
         System.out.println(isTheSameVersion("15", "15.0"));
         System.out.println(isTheSameVersion("10.1", "10.1.0"));
         System.out.println(isTheSameVersion("10.1", "10.1.1"));
